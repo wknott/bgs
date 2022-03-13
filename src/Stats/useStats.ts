eac24ed713208bf432bd1b28e7a38fbe5d65adf0
Wojciech <wojciechknott@gmail.com>
2022-03-13 13:53:49 +0100

    added useState template with one month results stats

---
+++ src/Stats/useStats.ts
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import Result from './ResultInterface';
+
+const useStats = () => {
+    const [results, setResults] = useState<Array<Result>>([]);
+    const [month, setMonth] = useState<string>('2022-03');
+    useEffect(() => {
+        const getResults = async () => {
+            const response = await fetch('https://wyniczek.herokuapp.com/api/results');
+            interface DataObject {
+                results: Result[];
+                numberOfResults: number;
+            }
+            const data: DataObject = await response.json();
+
+            setResults(data.results);
+        };
+
+        getResults();
+    }, [month]);
+
+    interface StatInterface {
+        gameName: string;
+        numberOfResults: number;
+        playingTime: number;
+    }
+
+    const getMonthStats = (monthDate: string): Array<StatInterface> => {
+        const startDate = new Date(monthDate);
+        const endDate = new Date(new Date(monthDate).setMonth(new Date(monthDate).getMonth() + 1));
+
+        const filteredResults = results.filter(
+            (result) => new Date(result.date) >= startDate && new Date(result.date) < endDate,
+        );
+
+        let stats: Array<StatInterface> = [];
+        filteredResults.forEach((result) => {
+            stats =
+                stats.filter((stat) => stat.gameName === result.game.name).length > 0
+                    ? stats.map((stat) =>
+                          stat.gameName === result.game.name
+                              ? {
+                                    ...stat,
+                                    numberOfResults: stat.numberOfResults + 1,
+                                    playingTime: stat.playingTime + (result.playingTime || 0),
+                                }
+                              : stat,
+                      )
+                    : [
+                          ...stats,
+                          { gameName: result.game.name, numberOfResults: 1, playingTime: result.playingTime || 0 },
+                      ];
+        });
+
+        return stats.sort((a, b) => (a.playingTime === b.playingTime ? 0 : a.playingTime > b.playingTime ? -1 : 1));
+    };
+
+    return {
+        monthStats: getMonthStats(month),
+        month,
+        setMonth,
+    };
+};
+
+export default useStats;
